fa2824c354a5b38e65a3f62162f2495ee5f56aee
Tyler Bowman <114122616+TylerBowman3@users.noreply.github.com>
2024-01-14 12:37:42 -0500

    submitting answers

---
+++ exercise.js
@@ -0,0 +1,36 @@
+// #1
+
+function createInstructor(first, last){
+    return {
+        first, 
+        last,
+    }
+};
+
+// #2 
+
+const favoriteNumber = 42;
+
+const instructor = {
+    [favoriteNumber]: "That is my favorite!"
+};
+
+// #3
+
+const instructor = {
+    firstName: 'Colt',
+    sayHi() {
+        return "Hi!";
+    },
+    sayBye() {
+        return instructor.firstName + "say bye!";
+    }
+}
+
+// #4
+
+function makeAnimal(species, verb, noise) {
+    return {
+        sp
+    }
+}
